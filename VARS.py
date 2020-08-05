5029d3d8e0db49ad29595d37d4fb2ab5d7149202
Sebastian <sebthomle@gmail.com>
2020-08-05 18:49:35 +0200

    Added example VARS.py file

---
+++ VARS.py
@@ -0,0 +1,6 @@
+CLIENT_ID = "70023ea2572048feb4d9e61782acd4e5" #Example ID replace with your own
+CLIENT_SECRET = "0ff44727f5174f90991e12206350cf9d" #Example SECRET replace with your own
+
+AUTH_TOKEN = "BQA_7-VjGc7NvCCLJ8O-xNRwpcqCzvGtbFCLXttk18jB9aCI5dK5itfksZJTYF0a4D5j7EgY1s1MQuGnR8Y" #Example TOKEN replace with your own
+
+PLAYLIST_ID = "5v7jrGldzzOWyI6WElQBL0" #Example ID replace with your own
